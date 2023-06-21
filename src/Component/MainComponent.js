5ff6f3dc2a15fab5f9d6dec0cc93f46259233f27
babaYogii <yogeshkodlinge@gamil.com>
2023-06-21 17:51:38 +0530

    Addded changes

--- src/Component/MainComponent.js
+++ src/Component/MainComponent.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useCallback } from 'react'
-import { Typography, Box, Card, Grid, CardMedia, CardContent } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import {Button, Typography,Box,Card, Grid,CardMedia,CardContent,CardActionArea,Chip} from '@mui/material'
 
 
 
@@ -7,17 +7,19 @@ import { Typography, Box, Card, Grid, CardMedia, CardContent } from '@mui/materi
 
 
 const MainComponent = (location) => {
+    
+    const [data,setData]=useState([]);
 
-  const [data, setData] = useState([]);
+    useEffect(()=>{
+        handelSearch()
+    },[location])
 
 
 
 
-
-
-  console.log(location)
-
-  const handelSearch = useCallback(async () => {
+   console.log(location)
+    
+const handelSearch=async()=>{
     const axios = require('axios');
 
     const options = {
@@ -34,60 +36,57 @@ const MainComponent = (location) => {
         lang: 'en_US'
       },
       headers: {
-        'X-RapidAPI-Key': '6f45ab08afmshd00259be7cf8d30p188666jsneb0539fc4f03',
+        'X-RapidAPI-Key': 'de6ab24f04msh131d8b9233f27a2p160f37jsn2809d354a633',
         'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
       }
     };
-
+    
     try {
-      const response = await axios.request(options);
-      setData(response.data.data)
-      // console.log(response)
-      // console.log(response.data, "waiting for response",location);
+        const response = await axios.request(options);
+        setData(response.data.data)
+        // console.log(response)
+        // console.log(response.data, "waiting for response",location);
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }, [location]);
-
-  useEffect(() => {
-    handelSearch()
-  }, [location, handelSearch])
-  console.log(data)
+}
+// if(data.length>0)
+console.log(data)
 
 
   return (
     <Grid container spacing={5}
-
+    
     >
+       
+        {
+            data.length>0 && data.map((place,ind)=>{
+             
+               return <Grid item  key={ind}   xs={4} 
+               
+              
+               > 
+                <Card elevation={6}>
+                  <CardMedia style={{height:250}}
+                  image={place.result_object.photo ? place.result_object.photo.images.large.url : "https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg"}
+                  title={place.result_object.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant='h5'>{place.result_object.name}</Typography>
+                    <Box display='flex' justifyContent='space-between'>
+                      <Typography variant='subtitle1'>Rating</Typography>
+                      <Typography variant='subtitle1' gutterBottom >{place.result_object.rating ?place.result_object.rating: "5.0" }</Typography>
 
-      {
-        data.length > 0 && data.map((place, ind) => {
+                    </Box>
+                  </CardContent>
 
-          return <Grid item key={ind} xs={4}
-
-
-          >
-            <Card elevation={6}>
-              <CardMedia style={{ height: 250 }}
-                image={place.result_object.photo ? place.result_object.photo.images.large.url : "https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg"}
-                title={place.result_object.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant='h5'>{place.result_object.name}</Typography>
-                <Box display='flex' justifyContent='space-between'>
-                  <Typography variant='subtitle1'>Rating</Typography>
-                  <Typography variant='subtitle1' gutterBottom >{place.result_object.rating ? place.result_object.rating : "5.0"}</Typography>
-
-                </Box>
-              </CardContent>
-
-
-            </Card>
-
-
-          </Grid>
-        })
-      }
+                  
+                </Card>
+                
+                
+                </Grid>
+            })
+        }
     </Grid>
   )
 }
